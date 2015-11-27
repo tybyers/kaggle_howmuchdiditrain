764017cdfe8edf2a4dff60924b52c079b7756be3
Tyler Byers <tybyers@gmail.com>
2015-11-27 11:39:28 -0800

    part 1, part 2 halfway

---
+++ kaggle_part2_modeling.R
@@ -0,0 +1,298 @@
+##--------------------------------------------
+##
+## Classifying Red and White Wine
+##
+## Class: Methods for Data Science at Scale
+## UW Data Science Certificate Class #3 of 3
+##
+## Tyler Byers
+## Nov 30, 2015
+##--------------------------------------------
+
+##-----Set working directory-----
+setwd('~/UW_DataScience/DataAtScale/final_project/')
+
+##-----Load Libraries-----
+library(dplyr); library(randomForest); library(rpart);
+library(caret); library(ggplot2)
+
+##-----Load Data Sets --------------
+train <- read.csv('./data/train.csv')
+test <- read.csv('./data/test.csv')
+
+## Filter out train$Expected 
+#train <- train %>% filter(Expected < 350)
+  ## Realized later that doing this messes up the scoring, so need to leave all values in.  
+  ## Maybe when training we can ignore it.
+
+## This is a feature engineering project.  First thing I'm going to do is just take the mean and 
+##  median of each radar reading and then model with that.  
+
+train_1perhr <- train %>% group_by(Id) %>%
+  summarise(n_obs = n(), radardist_km = first(radardist_km),
+            Ref_mean = mean(Ref, na.rm = TRUE), #Ref_med = median(Ref, na.rm = TRUE),
+            Ref_5x5_10th_mean = mean(Ref_5x5_10th, na.rm = TRUE),
+            Ref_5x5_50th_mean = mean(Ref_5x5_50th, na.rm = TRUE),
+            Ref_5x5_90th_mean = mean(Ref_5x5_90th, na.rm = TRUE),
+            RefComposite_mean = mean(RefComposite, na.rm = TRUE),
+            RefComposite_5x5_10th_mean = mean(RefComposite_5x5_10th, na.rm = TRUE),
+            RefComposite_5x5_50th_mean = mean(RefComposite_5x5_50th, na.rm = TRUE),
+            RefComposite_5x5_90th_mean = mean(RefComposite_5x5_90th, na.rm = TRUE),
+            RhoHV_mean = mean(RhoHV, na.rm = TRUE),
+            RhoHV_5x5_10th_mean = mean(RhoHV_5x5_10th, na.rm = TRUE),
+            RhoHV_5x5_50th_mean = mean(RhoHV_5x5_50th, na.rm = TRUE),
+            RhoHV_5x5_90th_mean = mean(RhoHV_5x5_90th, na.rm = TRUE),
+            Zdr_mean = mean(Zdr, na.rm = TRUE),
+            Zdr_5x5_10th_mean = mean(Zdr_5x5_10th, na.rm = TRUE),
+            Zdr_5x5_50th_mean = mean(Zdr_5x5_50th, na.rm = TRUE),
+            Zdr_5x5_90th_mean = mean(Zdr_5x5_90th, na.rm = TRUE),
+            Kdp_mean = mean(Kdp, na.rm = TRUE),
+            Kdp_5x5_10th_mean = mean(Kdp_5x5_10th, na.rm = TRUE),
+            Kdp_5x5_50th_mean = mean(Kdp_5x5_50th, na.rm = TRUE),
+            Kdp_5x5_90th_mean = mean(Kdp_5x5_90th, na.rm = TRUE),
+            Expected = first(Expected))
+
+## Function to check error
+mae <- function(actual, pred) {
+  mae <-  mean(abs(actual - pred))
+  print(paste('MAE: ', mae))
+  mae
+}
+
+write_output <- function(id, pred) {
+  output <- data.frame(Id = id, Expected = pred)
+  curdatetime <- strftime(Sys.time(), '%Y%m%d_%H%M%S')
+  write.csv(output, paste0('output/', curdatetime, '.csv'), row.names = FALSE)
+  print(paste0('Wrote file: ',  paste0('/output/', curdatetime, '.csv')))
+}
+
+## Going to do a baseline test using the mean rainfall for all observations
+mean_all <- mean(train_1perhr$Expected)  # mean of Expected with obs > 350 filtered
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], mean_all)
+# MAE: 11.77 -- hmm, doesn't seem right (later -- was with filtered Expected vals)
+#  With ugly bad Expeced vals, score is 27.2388, much more in line w/ Kaggle score.
+write_output(unique(test$Id), mean_all)
+# kaggle score: 27.82422
+
+## What if I take the mean of Expected, filtering out over 150, what then is my score?
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], 
+    mean((train_1perhr %>% filter(Expected < 150))$Expected))
+## Score: 24.5667
+
+median(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)])
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], 1.27)
+## Score: 23.53 --- would be top of leaderboard, seems too good to be true though
+write_output(unique(test$Id), 1.27)
+## Kaggle score: 24.1578, so it was too good to be true :)
+
+## Now need to figure out how to format the data so that we know how many minutes 
+##  each reading is good for.
+
+# Try the Murray-Palmer transformation, using the mean ref (nothing fancy)
+#  Still not doing real machine learning, just calculation, so don't need
+#  to do CV or anything like that. 
+mp_pred <- (10^(train_1perhr$Ref_mean/10)/200)^(5/8)
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], 
+    mp_pred[!is.na(mp_pred)])
+## Local score: 23.361129
+
+# Now do this calculation for the test set
+test_1perhr <- test %>% group_by(Id) %>% 
+  summarise(n_obs = n(), radardist_km = first(radardist_km),
+            Ref_mean = mean(Ref, na.rm = TRUE))
+mp_pred_test <- (10^(test_1perhr$Ref_mean/10)/200)^(5/8)
+## Fill in the NAs with 9999
+mp_pred_test[is.na(mp_pred_test)] <- 9999
+write_output(unique(test$Id), mp_pred_test)
+## Kaggle score: 24.01167, moved up to 377th place
+
+  
+## Want to do a spline-fit under the curve.
+## Can do a spline fit for each and every id
+system.time(Ref_spline <- sapply(unique(train$Id)[1:250], function(id) {
+  if(id %% 1000 == 1) {
+    print(paste(Sys.time(), ': Id = ', id))
+  }
+  #id_df <- train %>% filter(Id == id)
+  id_rows <- which(train$Id == id)
+  if(sum(!(is.na(train$Ref[id_rows]))) > 0) {
+    # do the calculation. Sort of a simple integration
+    sum(spline(x = train$minutes_past[id_rows],
+               y = train$Ref[id_rows], xout = 0:60)$y)/60
+  } else {
+    NA
+  }
+})
+)
+# The above is going to take around 26 hours to do. Need a different way.
+
+
+# Maybe take advantage of the group_by in dplyr
+ # do auc from MESS package....but cannot have NAs on the ends
+valid_ids <- train_1perhr$Id[which(!is.na(train_1perhr$Ref_mean))]
+system.time(Ref_spline <- train %>% filter(Id %in% valid_ids) %>%
+  group_by(Id) %>% 
+  summarise(Ref_spline_periodic = sum(spline(x = minutes_past,
+                                             y = Ref, xout = 0:60,
+                                             method = 'periodic')$y)/60))
+
+# This took 177 seconds! Sweet!  (with method = 'periodic')
+# However, it led to some horribly unbounded spline fits, leading to Inf
+#  results on the Murray Palmar fit.
+sfit_pred <-  (10^(Ref_spline$Ref_spline_periodic/10)/200)^(5/8)
+sfit_pred[sfit_pred > 200] <- 150
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], 
+    sfit_pred)
+# Local MAE: 23.4652
+
+# Try with a natural spline fit:
+Ref_spline_natural <- train %>% filter(Id %in% valid_ids) %>%
+  group_by(Id) %>% 
+  summarise(Ref_spline_natural = sum(spline(x = minutes_past,
+                                             y = Ref, xout = 0:60,
+                                             method = 'natural')$y)/60)
+sfit_pred_natural <-  (10^(Ref_spline_natural$Ref_spline_natural/10)/200)^(5/8)
+sfit_pred_natural[sfit_pred_natural > 150] <- 150
+mae(train_1perhr$Expected[!is.na(train_1perhr$Ref_mean)], 
+    sfit_pred_natural)
+## Local score: 24.727.  Maybe not worth submitting.  That method isn't so great.
+
+## Realized after looking at 4th posting on this page:
+##  https://www.kaggle.com/c/how-much-did-it-rain-ii/forums/t/16572/38-missing-data/
+## that I need to first convert Ref column to rain rate.  Maybe then I can find 
+## area under the curve and go from there.
+
+# Remove those dfs to reclaim space
+rm(Ref_spline); rm(Ref_spline_natural); rm(mp_pred); rm(mp_pred_test);
+rm(sfit_pred_natural); rm(sfit_pred); rm(valid_ids)
+
+# Calculate instantaneous rain rate (mm/hr):
+train$Ref_irr <- (10^(train$Ref/10)/200)^(5/8)
+## all the NAs should be 0 now, for 0 instant rain rate.
+train$Ref_irr[is.na(train$Ref_irr)] <- 0
+summary(train$Ref_irr)
+# Now do the auc calc, with a 0 anchor just before 0 and after 60
+system.time(train_Ref_irr_auc <- train %>% group_by(Id) %>%
+  summarise(n_obs = n(), 
+            Est_rain_linear = auc(x = c(-0.001, minutes_past, 60.001),
+                           y = c(0, Ref_irr, 0), type = 'linear')/60#,
+            #Est_rain_spline = auc(x = c(-0.001, minutes_past, 60.001),
+            #               y = c(0, Ref_irr, 0), type = 'spline')/60
+  )
+)
+train_Ref_irr_auc$Est_rain_spline[train_Ref_irr_auc$Est_rain_spline < 0] <- 0
+## only problem is that for the 1-observation hours we're getting NA,
+##  so added anchors on either side of 0 and 60.
+scorerows <- !is.na(train_1perhr$Ref_mean)
+mae(train_1perhr$Expected[scorerows], 
+    train_Ref_irr_auc$Est_rain_linear[scorerows])
+# Calc MAE: 23.3873.  Slightly lower than my "mean" method, but same area.
+mae(train_1perhr$Expected[scorerows], 
+    train_Ref_irr_auc$Est_rain_spline[scorerows])
+# Spline calc MAE: 36.883.  This gets pretty unbounded, so going to use 'linear' 
+#  from here on out.
+
+# That MAE of 23.3873 is close to that for my "mean" calculation. Not going to 
+#  submit to Kaggle at this time.  
+# But, want to do some modeling with all the Ref values and the km from gauge.
+#  Maybe can start to get better results with that. 
+
+# Want a function with the MP transformation
+mp_transform <- function(Ref_vect) {
+  (10^(Ref_vect/10)/200)^(5/8)
+}
+
+train_transf <- as.data.frame(lapply(train[,5:11], function(x) { mp_transform(x) }))
+names(train_transf) <- paste0(names(train_transf), '_irr')
+summary(train_transf)
+train_transf <- as.data.frame(lapply(train_transf, function(x) {
+  x[is.na(x)] <- 0
+  x
+}))
+train_transf <- bind_cols(train[c('Id', 'minutes_past', 'radardist_km', 'Ref_irr')],
+                          train_transf, train[c('Expected')])
+# Now do AUC for each column. 
+train_allref_auc <- train_transf %>% group_by(Id) %>%
+  summarise(n_obs = n(), radardist_km = first(radardist_km), 
+            Ref_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                  y = c(0, Ref_irr, 0), type = 'linear')/60,
+            Ref_5x5_10th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                   y = c(0, Ref_5x5_10th_irr, 0), type = 'linear')/60,
+            Ref_5x5_50th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                   y = c(0, Ref_5x5_50th_irr, 0), type = 'linear')/60,
+            Ref_5x5_90th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                   y = c(0, Ref_5x5_90th_irr, 0), type = 'linear')/60,
+            RefComposite_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                   y = c(0, RefComposite_irr, 0), type = 'linear')/60,
+            RefComposite_5x5_10th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                   y = c(0, RefComposite_5x5_10th_irr, 0), 
+                                   type = 'linear')/60,
+            RefComposite_5x5_50th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                            y = c(0, RefComposite_5x5_50th_irr, 0), 
+                                            type = 'linear')/60,
+            RefComposite_5x5_90th_auc = auc(x = c(-0.001, minutes_past, 60.001),
+                                            y = c(0, RefComposite_5x5_90th_irr, 0), 
+                                            type = 'linear')/60
+  )
+
+
+# Need to know which Ids are all-NA Ref (don't train or score on these)
+# Need to know which Ids have Expected > 250 (too high of readings)
+allrefna_train <- train_1perhr$Id[is.na(train_1perhr$Ref_mean)]
+highExpected_train <- train_1perhr$Id[train_1perhr$Expected > 250]
+
+# Modeling (remember to filter out Expected above 200 when training), and filter
+#  out the Ids with all NA Ref values when training.
+
+
+run_mods <- function(data, allrefna, highExpected, fol, mod = 'rpart', k = 10) {
+  print(paste(Sys.time(), ': Entering run_mods function'))
+  folds <- createFolds(data$Expected, k = k)
+  i <- 0
+  accuracies <- lapply(folds, function(fold) {
+    i <<- i + 1
+    train <- data[-fold, ]
+    print(paste('Fold: ', i))
+    #print(paste('nrow(train):', nrow(train)))
+    train_filt <- train %>% filter(!(Id %in% allrefna)) %>% 
+      filter(!(Id %in% highExpected))
+    #print(paste('nrow(train_filt):', nrow(train_filt)))
+    test <- data[fold, ]
+    if(mod == 'rpart') {
+      cart <- rpart(fol, data = train_filt)
+      pred <- predict(cart, newdata = test)
+      
+    } else if(mod == 'rf') {
+      rf <- randomForest(fol, data = train_filt)
+      print(importance(rf))
+      pred <- predict(rf, newdata = test)
+    }
+    pred_df <- data.frame(Id = test$Id, pred = pred)
+    #print(paste('nrow(pred_df)', nrow(pred_df)))
+    pred_df$Expected <- test$Expected
+    pred_df <- pred_df %>% filter(!(Id %in% allrefna))
+    #print(paste('nrow(pred_df) after filter', nrow(pred_df)))
+    mae_score <- mae(pred_df$Expected, pred_df$pred)
+    print(paste(Sys.time(), ': MAE #',i,':', mae_score))
+    mae_score
+  })
+  print(paste('Average MAE with 10-fold cv:', 
+              sum(unlist(accuracies))/k))
+  accuracies
+}
+
+
+train_allref_auc$Expected <- train_1perhr$Expected
+
+fol <- Expected ~ radardist_km + Ref_auc + Ref_5x5_10th_auc + 
+  Ref_5x5_50th_auc + Ref_5x5_90th_auc + RefComposite_auc +
+  RefComposite_5x5_10th_auc + RefComposite_5x5_50th_auc +
+  RefComposite_5x5_90th_auc
+
+rparts <- 
+  run_mods(train_allref_auc, allrefna_train,
+           highExpected_train, fol, mod = 'rpart', k = 3)
+# rpart tree gives an MAE of: .   Not too good
+# Try a randomForest
+library(randomForest)
+run_mods(train_allref_auc, allrefna_train, highExpected_train, fol, mod = 'rf')
